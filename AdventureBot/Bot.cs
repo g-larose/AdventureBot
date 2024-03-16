7f53d2d7c254838c870d0a4d0fca66ca17f8c672
g-larose <larose.g@fromtier.com>
2024-03-16 18:53:21 -0400

    Initial :tada:

---
+++ AdventureBot/Bot.cs
@@ -0,0 +1,24 @@
+﻿using AdventureBot.Models;
+using Guilded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AdventureBot
+{
+    public class Bot
+    {
+        private static readonly string? json = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json"));
+        private static readonly string? token = JsonSerializer.Deserialize<ConfigJson>(json!)!.Token!;
+        private static readonly string? prefix = JsonSerializer.Deserialize<ConfigJson>(json!)!.Prefix!;
+        private static readonly string? timePattern = "hh:mm:ss tt";
+
+        public async Task RunAsync()
+        {
+            await using var client = new GuildedBotClient(token!);
+        }
+    }
+}
---
+++ AdventureBot/Models/ConfigJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AdventureBot.Models
+{
+    public class ConfigJson
+    {
+        [JsonPropertyName("token")]
+        public string? Token { get; set; }
+        [JsonPropertyName("prefix")]
+        public string? Prefix { get; set; }
+        [JsonPropertyName("connectionstring")]
+        public string? ConnectionString { get; set; }
+    }
+}
