54d566cf1195bcf4ae0862c34b4bc076e934ac53
g-larose <larose.g@fromtier.com>
2024-03-17 07:19:44 -0400

    added GameDataproviderService

---
+++ AdventureBot/Interfaces/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using MODiX.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureBot.Interfaces
+{
+    public interface IDataProvider
+    {
+        Result<string, string>? SetPlayerProfessionAvatar(string profession);
+    }
+}
---
+++ AdventureBot/Services/GameDataProviderService.cs
@@ -0,0 +1,23 @@
+﻿using AdventureBot.Interfaces;
+using MODiX.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureBot.Services
+{
+    public class GameDataProviderService : IDataProvider
+    {
+        public Result<string, string>? SetPlayerProfessionAvatar(string? profession)
+        {
+            var result = profession switch
+            {
+                "hunter" => Result<string, string>.Ok("https://cdn.gilcdn.com/MediaChannelUpload/8607290cae1361b957161eb1e52a8ce5-Full.webp?w=2500&h=2500"),
+                _ =>  Result<string, string>.Err("could not set player profession")
+            };
+            return result!;
+        }
+    }
+}
